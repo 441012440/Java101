c3f72e3d55068512c16be6c7d307adbf2e430659
loai7 <91420944+loai7@users.noreply.github.com>
2021-10-11 23:18:28 -0700

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {  
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int mynum = scan.nextint;
+        if (mynum %=2)   ;
+        System.out.println("Odd");
+        else if (mynum %=1)  ;
+        System.out.println("Even")
+    }
+}
