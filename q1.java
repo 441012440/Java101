b2b77d8659a53a11d7881fe3535c8b9bbb8f1f82
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-10-19 08:36:09 +0300

    Update q1.java

--- q1.java
+++ q1.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int mynum = scan.nextint;
-        if (mynum %=2)   ;
-        System.out.println("Odd");
-        else if (mynum %=1)  ;
-        System.out.println("Even")
+        if (mynum %=2 == 0)   ;
+        System.out.println("Even");
+        else ;
+        System.out.println("Odd")
     }
 }
