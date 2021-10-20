7e378161e7a7fe32409c7328fd96e39331c908e3
loai nahari (441012440) <91420944+loai7@users.noreply.github.com>
2021-10-20 11:02:37 -0700

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Mid {  
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float Mynum = scan.nextFloat();
+        
+        System.out.println(Mynum);
+        Mynum /= 10  ;
+        System.out.println(Mynum);
+        Mynum *=100 ;
+        System.out.println(Mynum);
+    
+        if (Mynum < 0 || Mynum > 100);
+        System.out.println("Eror,please try again and enter between 0 and 100");
+        
+        else if (Mynum < 50)
+        System.out.println(Mynum "is less than 50%")
+        
+        else (Mynum > 50);
+        System.out.println(Mynum"is greater or equal than 50%");
+        
+        
+	}
+}
